ebd6ae68f8857ea704ab7feef24b4a6e6fb81489
Jang Whemoon <palindrom615@gmail.com>
2022-10-02 00:39:05 +0900

    Package lookup crate

--- src/lookup/mod.rs
+++ src/lookup/mod.rs
@@ -1,7 +1,11 @@
+use log::info;
+use pnet::datalink::interfaces;
 use pnet::util::MacAddr;
 use std::net::Ipv4Addr;
 
 pub fn lookup_ipv4(ipv4_addr: Ipv4Addr) -> MacAddr {
+    let interface = interfaces();
+    info!("{:?}", interface);
     panic!("IPv4 Not Implemented, {}", ipv4_addr)
 }
 
