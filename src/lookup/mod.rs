027366af3f56ff70d2d7296ee98cb4897ad0237e
Jang Whemoon <palindrom615@gmail.com>
2022-10-03 19:43:57 +0900

    Implement wake by hostname

--- src/lookup/mod.rs
+++ src/lookup/mod.rs
@@ -1,5 +1,7 @@
 use pnet::util::MacAddr;
+use std::net::IpAddr::V4;
 use std::net::Ipv4Addr;
+use std::net::ToSocketAddrs;
 
 use crate::lookup::arp::get_mac_addr;
 
@@ -10,5 +12,28 @@ pub fn lookup_ipv4(ipv4_addr: Ipv4Addr) -> MacAddr {
 }
 
 pub fn lookup_hostname(hostname: String) -> MacAddr {
-    panic!("Hostname Not Implemented, {}", hostname)
+    match (hostname, 0)
+        .to_socket_addrs()
+        .expect("Host not resolvable")
+        .next()
+        .expect("Host not resolvable")
+        .ip()
+    {
+        V4(addr) => lookup_ipv4(addr),
+        _ => panic!("Not Implemented for IP v4"),
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use log::debug;
+    use std::net::ToSocketAddrs;
+    fn init() {
+        let _ = env_logger::builder().is_test(true).try_init();
+    }
+    #[test]
+    fn test_resolve() {
+        init();
+        debug!("{:?}", ("google.com", 0).to_socket_addrs());
+    }
 }
