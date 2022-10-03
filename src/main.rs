3a439d5f83087acd57a36c9aa6fcdbaab9eb076d
Jang Whemoon <palindrom615@gmail.com>
2022-10-03 20:06:06 +0900

    Refactor main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use pnet::util::MacAddr;
-use std::env;
+use std::env::args;
 use std::net::Ipv4Addr;
 
 use std::str::FromStr;
@@ -9,28 +9,17 @@ use wake_neighbor::send::MagicPacket;
 fn main() {
     env_logger::init();
 
-    let arg1 = &env::args().nth(1).expect("no destination");
-
-    let mac_addr = match parse_arg(arg1) {
-        Destination::Mac(mac_addr) => mac_addr,
-        Destination::Ipv4(ip_addr) => lookup_ipv4(ip_addr),
-        Destination::HostName(hostname) => lookup_hostname(hostname),
-    };
-    MagicPacket::new(mac_addr).send();
-}
-
-fn parse_arg(arg1: &str) -> Destination {
-    if let Ok(mac_addr) = MacAddr::from_str(arg1) {
-        Destination::Mac(mac_addr)
-    } else if let Ok(ipv4_addr) = Ipv4Addr::from_str(arg1) {
-        Destination::Ipv4(ipv4_addr)
-    } else {
-        Destination::HostName(String::from(arg1))
+    fn lookup_hw_addr(arg: &str) -> MacAddr {
+        if let Ok(mac_addr) = MacAddr::from_str(arg) {
+            mac_addr
+        } else if let Ok(ipv4_addr) = Ipv4Addr::from_str(arg) {
+            lookup_ipv4(ipv4_addr)
+        } else {
+            lookup_hostname(String::from(arg))
+        }
     }
-}
 
-enum Destination {
-    Mac(MacAddr),
-    Ipv4(Ipv4Addr),
-    HostName(String),
+    let arg1 = &args().nth(1).expect("no destination");
+
+    MagicPacket::new(lookup_hw_addr(arg1)).send();
 }
